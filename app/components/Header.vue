3ec6c9f5b3e40b87cffd0990e58327049e018b8d
Bruno Gonzales <holachepi@gmail.com>
2026-02-07 22:56:21 -0500

    update repo url

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,10 +1,10 @@
 <script setup>
-const repositoryUrl = "#"
+const repositoryUrl = "https://github.com/br1gonzales/human-forest"
 </script>
 
 <template>
     <header class="flex sticky top-0 w-full relative p-[12px_16px] items-center">
         <Logo />
-        <NuxtLink class="ml-auto hover:underline" :to="repositoryUrl">repository</NuxtLink>
+        <NuxtLink target="_blank" class="ml-auto hover:underline" :to="repositoryUrl">repository</NuxtLink>
     </header>
 </template>
